f254edf25f00d615f8ca1425411b44d7242ac853
Emil Wall <emil.wall@valtech.se>
2019-09-21 20:26:27 +0200

    WIP: collapse effect

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,26 @@ AOS.init();
 nav = document.getElementsByTagName("nav")[0];
 var showWhenScrolled = function() {
   var y = window.scrollY;
-  if (y != 0) {
+  if (y == 0 && nav.className.indexOf('hide') === -1) {
+    nav.classList.add("hide");
+  } else if (y != 0) {
     nav.classList.remove("hide");
-    window.removeEventListener("scroll", showWhenScrolled);
   }
 };
 window.addEventListener("scroll", showWhenScrolled);
+showWhenScrolled();
+
+divs = document.getElementsByClassName("toggle-collapse");
+var toggleCollapse = function() {
+  var details = document.getElementById("collapse-" + this.id);
+  if (details.className.indexOf('collapse-hide') === -1) {
+    details.classList.add("collapse-hide");
+    details.classList.remove("collapse-show");
+  } else {
+    details.classList.add("collapse-show");
+    details.classList.remove("collapse-hide");
+  }
+};
+for (var i=0; i<divs.length; i++) {
+  divs[i].addEventListener("click", toggleCollapse);
+}
